e572eb3cd63f6c8d109b4b47394c0fef844757f0
roopahn184 <rooparoopahn184@gmail.com>
2023-06-15 13:15:45 +0530

    ok

---
+++ devgray/Practice1.js
@@ -0,0 +1,16 @@
+// console.log("Roopa".charAt(4));
+
+switch(Math.floor(Math.random()*3 + 1)){
+    case 1:
+        console.log("1");   
+        break;
+        case 2:
+            console.log("2");   
+            break; 
+            case 3:
+                console.log("3");   
+        break;
+        default:
+            console.log("no match");   
+
+}
