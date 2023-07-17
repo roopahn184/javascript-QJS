971655f4e26250d98a66a98435c60bbf1cc797f5
roopahn184 <rooparoopahn184@gmail.com>
2023-07-17 16:06:20 +0530

    ok

---
+++ javascript info/Main1.js
@@ -0,0 +1,11 @@
+//variables//
+let message = "good evening"
+alert( message);
+
+let user = "john", age = "22", sal =25000;
+alert(user)
+
+ let admin, name;
+ name = "john"
+  admin = name;
+  alert(admin);
