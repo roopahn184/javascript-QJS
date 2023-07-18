907904e4c421776ba59dbad3576fd910d3a7ba34
roopahn184 <rooparoopahn184@gmail.com>
2023-07-18 14:09:57 +0530

    ok

--- javascript info/Main2.js
+++ javascript info/Main2.js
@@ -1,3 +1,15 @@
+var myArray = ["john", "jan", "coffee"]
+myArray.push(["happy", "sad"]);
+console.log(myArray);
+
 var myArray = [1,2,3]
 var removedArray = myArray.pop()
-comsole.log(myArray)
+console.log(myArray)
+
+var ourArray =["stimpson", "j", ["cat"]];
+var removedArray = ourArray.shift();
+console.log(ourArray);
+
+ourArray.unshift("happy");
+console.log(ourArray);
+
