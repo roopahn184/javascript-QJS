855a2353c2ae3921234d82aea544cdb0748939a1
roopahn184 <rooparoopahn184@gmail.com>
2023-07-19 12:05:05 +0530

    ok

--- javascript info/Main2.js
+++ javascript info/Main2.js
@@ -13,3 +13,29 @@ console.log(ourArray);
 ourArray.unshift("happy");
 console.log(ourArray);
 
+//passing function as a argument
+
+function ourfunctionwithArgs(a, b){
+    console.log(a-b);
+}
+ourfunctionwithArgs(10, 5)
+
+//global scope and functions
+var myGlobal = 10;
+function fun1(){
+    oopsGlobal = 5;
+}
+
+function fun2(){
+    var output ="";
+    if(typeof myGlobal != "undefined"){
+       output+="myGlobal:" + myGlobal;
+    }
+    if(typeof oopsGlobal != "undefined"){
+        output +="oopsGlobal:" +oopsGlobal;
+    }
+    console.log(output);
+}
+
+fun1()
+fun2()
